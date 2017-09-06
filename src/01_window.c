424d999c0c882487423ca3f648df490392b1858a
Peng Li <seudut@gmail.com>
2017-09-06 14:42:20 +0800

    format the code

--- src/01_window.c
+++ src/01_window.c
@@ -16,27 +16,32 @@ int main (int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
     if (window == NULL)
     {
 	printf("Could not create window: %s\n", SDL_GetError());
-	return 1;
+	return EXIT_FAILURE;
     }
 
-    SDL_Event event;
+    SDL_Event windowEvent;
 
     while (1)
     {
-	while (SDL_PollEvent(&event)) {
+	// On Mac, if SDL_PollEvent is not called, the window won't show up, but on Linux it can.
+	// Not sure why, http://www.dreamincode.net/forums/topic/390039-os-x-and-sdl20-window-does-not-show-up/
+	if (SDL_PollEvent(&windowEvent)) {
+	    if (SDL_QUIT == windowEvent.type)
+		break;
 	}
     }
 
-
+    // Destory window
     SDL_DestroyWindow(window);
+    // Quit SDL substems
     SDL_Quit();
     
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
