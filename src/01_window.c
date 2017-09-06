e3cd32ba8fe45070ed3c15c98f82cc61a955a4e0
Peng Li <seudut@gmail.com>
2017-09-06 10:30:23 +0800

    Add event loop for window

--- src/01_window.c
+++ src/01_window.c
@@ -8,6 +8,7 @@
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
+const char* WINDOW_TITLE = "Hello SDL Window";
 
 int main (int argc, char *argv[])
 {
@@ -15,7 +16,7 @@ int main (int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("Hello SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
     if (window == NULL)
     {
@@ -23,7 +24,15 @@ int main (int argc, char *argv[])
 	return 1;
     }
 
-    SDL_Delay(3000);
+    SDL_Event event;
+
+    while (1)
+    {
+	while (SDL_PollEvent(&event)) {
+	}
+    }
+
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     
